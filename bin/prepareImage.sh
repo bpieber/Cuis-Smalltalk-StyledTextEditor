d4c9248a6bda72617a60640aa52b3181b3edad1c
Bernhard Pieber <bernhard@pieber.com>
2013-07-28 21:16:03 +0200

    honour new repository name in shell script

--- bin/prepareImage.sh
+++ bin/prepareImage.sh
@@ -2,5 +2,5 @@
 NAME="$1"
 BUILD="$2"
 cd $NAME
-ditto ../Cuis-StyledTextEditor/ClipboardExtendedPlugin.bundle/ Cog.$BUILD.app/Contents/Resources/ClipboardExtendedPlugin.bundle/
+ditto ../Cuis-Smalltalk-StyledTextEditor/ClipboardExtendedPlugin.bundle/ Cog.$BUILD.app/Contents/Resources/ClipboardExtendedPlugin.bundle/
 mv Cog.$BUILD.app Cog.$BUILD.ClipboardExtendedPlugin.app
