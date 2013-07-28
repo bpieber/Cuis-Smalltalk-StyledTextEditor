4478e2e809037820a49364ed13442fe6792bc1da
Bernhard Pieber <bernhard@pieber.com>
2013-07-28 21:07:34 +0200

    add shell script to prepare an image for StyledTextEditor

--- bin/prepareImage.sh
+++ bin/prepareImage.sh
@@ -3,4 +3,4 @@ NAME="$1"
 BUILD="$2"
 cd $NAME
 ditto ../Cuis-StyledTextEditor/ClipboardExtendedPlugin.bundle/ Cog.$BUILD.app/Contents/Resources/ClipboardExtendedPlugin.bundle/
-tar -xf ../Cuis-StyledTextEditor/scowl-7.1.tar.gz
+mv Cog.$BUILD.app Cog.$BUILD.ClipboardExtendedPlugin.app
